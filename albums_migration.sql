6b1123b4ec42b90b0bab4af1534a1359c64ad406
Ignacio Mares <ignacio.mares00@gmail.com>
2021-10-01 15:34:28 -0500

    updated info on the albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
-                      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                      artist VARCHAR(35) NOT NULL,
-                      name VARCHAR(35) NOT NULL,
-                      release_date INT,
-                      sales FLOAT NOT NULL DEFAULT 0,
-                      genre VARCHAR(35) NOT NULL,
-                      PRIMARY KEY (id)
+      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+      artist VARCHAR(128),
+      name VARCHAR(128),
+      release_date SMALLINT UNSIGNED,
+      sales FLOAT(6, 2),
+      genre VARCHAR(128),
+      PRIMARY KEY (id)
 );
