9ea857dd11867a8ca24b5e735dd7074e8cd5b62f
Ignacio Mares <ignacio.mares00@gmail.com>
2021-10-04 11:05:53 -0500

    added album seeder file and quotes seeder and statement lecture notes

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,8 +2,8 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-      artist VARCHAR(128),
-      name VARCHAR(128),
+      artist VARCHAR(128) NOT NULL,
+      name VARCHAR(128) NOT NULL,
       release_date SMALLINT UNSIGNED,
       sales FLOAT(6, 2),
       genre VARCHAR(128),
---
+++ select_exercises.sql
@@ -0,0 +1,6 @@
+USE codeup_test_db;
+
+-- The name of all albums by Pink Floyd.
+SELECT 'I am output!' AS 'Info';
+
+SELECT artist FROM albums;
