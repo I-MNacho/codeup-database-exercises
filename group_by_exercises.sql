97ace454feb335c8da793421df48b227190bc4b1
Ignacio Mares <ignacio.mares00@gmail.com>
2021-10-06 10:39:58 -0500

    added aliases exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -6,8 +6,9 @@ SHOW TABLES;
 SELECT DISTINCT title FROM titles;
 
 #3. Find your query for employees whose last names start and end with 'E'. Update the query to find just the unique last names that start and end with 'E' using GROUP BY.
-SELECT DISTINCT last_name FROM employees
+SELECT last_name FROM employees
 WHERE last_name LIKE 'E%e'
+GROUP BY last_name
 ORDER BY last_name;
 
 #4. Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
@@ -20,12 +21,13 @@ WHERE last_name LIKE '%q%'
 AND last_name NOT LIKE '%qu%';
 
 #6.Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
-SELECT DISTINCT last_name COUNT(*) FROM employees
+SELECT COUNT(*), last_name FROM employees
 WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%';
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
 
 
 #7. Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names.
-SELECT COUNT(*) FROM employees
+SELECT COUNT(*), gender FROM employees
 WHERE first_name IN ('Irena', 'Vidya','Maya')
-ORDER BY first_name;
+GROUP BY gender;
